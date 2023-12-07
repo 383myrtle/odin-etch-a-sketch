607cc4a5481db78214bef1b4f3068c2fc27d0a86
383myrtle <89387525+383myrtle@users.noreply.github.com>
2023-12-07 15:10:47 +0200

    Add block class to each block

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let div = document.querySelector('.container')
 for (let i = 0; i < 256; i++){
     let block = document.createElement('div')
+    block.classList.add('block')
     div.appendChild(block)
 }
