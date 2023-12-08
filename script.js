e2770b299995296cdc7419f57862227836f10e98
383myrtle <89387525+383myrtle@users.noreply.github.com>
2023-12-08 12:26:36 +0200

    Add button to create new grid of custom size

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ for (let i = 0; i < 256; i++){
 function changeColor(e){
     e.target.classList.add('blue')
 }
+
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
+btn.addEventListener('click', createNewGrid)
+
+function createNewGrid(e){
+    let size = input.value
+    input.value = ''
+    div.textContent = ''
+
+    for (let i = 0; i < size*size; i++){
+        let block = document.createElement('div')
+        block.classList.add('block')
+        block.style.width = 960/size + "px"
+        block.style.height = 960/size + "px"
+        div.appendChild(block)
+        block.addEventListener('mouseover', changeColor)
+    }
+}
