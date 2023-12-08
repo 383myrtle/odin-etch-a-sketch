2f7b5a0668a42dc5a52086a0d3b9eeec671ed87f
383myrtle <89387525+383myrtle@users.noreply.github.com>
2023-12-08 11:59:18 +0200

    Change block color on mouse hover

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ for (let i = 0; i < 256; i++){
     let block = document.createElement('div')
     block.classList.add('block')
     div.appendChild(block)
+    block.addEventListener('mouseover', changeColor)
+}
+
+function changeColor(e){
+    e.target.classList.add('blue')
 }
